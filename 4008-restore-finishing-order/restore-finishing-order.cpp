3d084cf71698e17ed7f0caf0c7a8ccc525796632
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2025-08-31 23:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (86.45%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        int n=order.size();
+        int m=friends.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(order[i]==friends[j])
+                {
+                    ans.push_back(friends[j]);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
