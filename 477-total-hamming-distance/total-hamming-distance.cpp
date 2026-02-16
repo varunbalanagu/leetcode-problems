e59eb14a1617549662af90651a302a388f7041ba
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2026-02-16 20:07:14 +0530

    Time: 172 ms (15.68%) | Memory: 86 MB (16.42%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    int fun(int ans)
-    {
-        return __builtin_popcount(ans);
-    }
     int totalHammingDistance(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
+       int n=nums.size();
+       vector<int>sol(32);
+       for(int i=0;i<n;i++)
+       {
+        int ans=nums[i];
+        vector<int>binary;
+        while(ans>0)
         {
-            for(int j=i+1;j<n;j++)
+            binary.push_back(ans%2);
+            ans=ans/2;
+        }
+        while(binary.size()<32)
+        {
+            binary.push_back(0);
+        }
+        reverse(binary.begin(),binary.end());
+        int c=0;
+        for(int j=0;j<32;j++)
+        {
+            if(binary[j]==1)
             {
-                sum=sum+fun(nums[i]^nums[j]);
+                sol[j]++;
             }
         }
-         return sum;
+       }
+       int res=0;
+       for(int i=0;i<32;i++)
+       {
+        // cout<<sol[i];
+         res+=sol[i]*(n-sol[i]);
+       }
+       return res;
+
     }
 };
