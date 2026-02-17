93812a325f739bb052c353a8499db3afce7c2d03
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2026-02-17 14:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (95.48%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int low=0,high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+            if(arr[low]<=arr[mid])
+            {
+                 if(target>=arr[low]&&target<arr[mid])
+                 {
+                    high=mid-1;
+                 }
+                 else
+                 {
+                    low=mid+1;
+                 }
+            }
+            else
+            {
+                if(target>arr[mid]&&target<=arr[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+
+        }
+        return -1;
+        
+    }
+};
