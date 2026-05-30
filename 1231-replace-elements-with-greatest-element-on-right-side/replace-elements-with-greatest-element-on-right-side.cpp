f64389eda20304766ac913658dd5092641c8058f
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2026-05-30 16:53:41 +0530

    Time: 3 ms (47.33%) | Memory: 72.9 MB (19.72%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n=arr.size(),m;
-        vector<int> ans(n,-1);
-        for(int i=0;i<n;i++)
-        {
-            m=-1;
-            for(int j=i+1;j<n;j++){
-                m=max(m,arr[j]);
+        int n = arr.size();
+        vector<int> ans(n);
+
+        stack<int> st;
+        st.push(-1);   // for the last element
+
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] = st.top();
+
+            if(st.top() < arr[i]) {
+                st.push(arr[i]);
             }
-            ans[i]=m;
         }
+
         return ans;
     }
 };
