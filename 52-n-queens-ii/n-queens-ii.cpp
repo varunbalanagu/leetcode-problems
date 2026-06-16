b8229d3917fedd0b95f7a8df48db422bbebd1c43
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2026-06-16 14:59:53 +0530

    Time: 3 ms (35.70%) | Memory: 9.5 MB (13.68%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+   bool check(int row,int col,vector<string>&board,int n)
+   {
+         int crow=row;
+         int ccol=col;
+         while(crow>=0&&ccol>=0)
+         {
+            if(board[crow][ccol]=='Q')
+            {
+                return false;
+            }
+            crow--;
+            ccol--;
+         }
+         crow=row;
+          ccol=col;
+           while(crow<n&&ccol>=0)
+         {
+            if(board[crow][ccol]=='Q')
+            {
+                return false;
+            }
+            crow++;
+            ccol--;
+         }
+         crow=row;
+          ccol=col;
+          while(ccol>=0)
+          {
+            if(board[crow][ccol]=='Q')
+            {
+                return false;
+            }
+            ccol--;
+          }
+          return true;
+
+   }
+    void identify(int col,vector<string>&board,vector<vector<string>>&sol,int n){
+        if(col==n)
+        {
+            sol.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++)
+        {
+            if(check(row,col,board,n))
+            {
+                board[row][col]='Q';
+                identify(col+1,board,sol,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string>board(n);
+        string s(n,'.');
+        vector<vector<string>>sol;
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        identify(0,board,sol,n);
+        map<vector<string>,int>mp;
+        int c=0;
+        for(int i=0;i<sol.size();i++)
+        {
+            if(mp.find(sol[i])!=mp.end())
+            {
+                continue;
+            }
+            else
+            {
+                c++;
+                mp[sol[i]]++;
+            }
+        }
+        return c;
+    }
+};
