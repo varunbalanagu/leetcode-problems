6217124a233427b5e0126aa23ce51dff6c3c1f0c
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2026-06-23 14:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (83.51%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,60 @@
+class Disjoint{
+    vector<int>rank,parent;
+public:
+  Disjoint(int n){
+    rank.resize(n+1,0);
+    parent.resize(n+1);
+    for(int i=0;i<=n;i++)
+    {
+        parent[i]=i;
+    }
+  }
+  int FindParent(int node){
+    if(node==parent[node])
+    return node;
+    return parent[node]=FindParent(parent[node]);
+  }
+  void FindByUnion(int u,int v)
+  {
+    int ulp_u=FindParent(u);
+    int ulp_v=FindParent(v);
+    if(ulp_u==ulp_v) return;
+    if(rank[ulp_u]<rank[ulp_v])
+    {
+        parent[ulp_u]=ulp_v;
+    }
+    else if(rank[ulp_v]<rank[ulp_u])
+    {
+        parent[ulp_v]=ulp_u;
+    }
+    else
+    {
+        parent[ulp_v]=ulp_u;
+        rank[ulp_u]++;
+    }
+  }
+};
+
+
 class Solution {
 public:
-    void DFS(int i,vector<vector<int>>&adj,vector<bool>&vis)
-    {
-        vis[i]=true;
-        for(auto it:adj[i])
-        {
-            if(!vis[it])
-            {
-                DFS(it,adj,vis);
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<vector<int>>adj(n);
+         Disjoint disjoint(n);
+         int c=0;
         for(int i=0;i<n;i++)
         {
-           for(int j=0;j<n;j++)
-           {
-            if(isConnected[i][j]==1&&i!=j)
-            {
-                adj[i].push_back(j);
-                adj[j].push_back(i);
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+            disjoint.FindByUnion(i,j);
+                }
             }
-           }
         }
-        vector<bool>vis(n,false);
-        int c=0;
         for(int i=0;i<n;i++)
         {
-            if(vis[i]==false)
+            if(disjoint.FindParent(i)==i)
             {
                 c++;
-                DFS(i,adj,vis);
             }
         }
         return c;
