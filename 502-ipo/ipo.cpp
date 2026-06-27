a9ccda18279482db6ac6c0a5c6198335c488a146
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-06-27 12:46:49 +0530

    Time: 77 ms (46.87%) | Memory: 131.5 MB (30.88%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -3,6 +3,7 @@ public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
 
         vector<pair<int,int>> projects;
+        priority_queue<int> pq;
 
         for(int i = 0; i < profits.size(); i++)
         {
@@ -11,24 +12,22 @@ public:
 
         sort(projects.begin(), projects.end());
 
-        priority_queue<int> pq;
-
         int n = projects.size();
-        int idx = 0;
+        int i = 0;
 
         while(k--)
         {
-            // Push all projects that can be started
-            while(idx < n && projects[idx].first <= w)
+            for(; i < n; i++)
             {
-                pq.push(projects[idx].second);
-                idx++;
+                if(projects[i].first <= w)
+                    pq.push(projects[i].second);
+                else
+                    break;
             }
 
             if(pq.empty())
                 break;
 
-            // Take the maximum profit
             w += pq.top();
             pq.pop();
         }
