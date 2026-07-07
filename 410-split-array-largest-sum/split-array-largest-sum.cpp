977589d7642f5c39ed7f79bc3662a72cfab565d9
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-07-07 10:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (42.41%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool good(vector<int>& nums, int mid, int k) {
+        int sum =0;
+        int temp=0,temp2=0;
+        int parts=1;
+        int n = nums.size();
+        for(int i =0 ;i <nums.size();i++)
+        {
+           if(nums[i]>mid)
+           {
+            return false;
+           } 
+           if(sum+nums[i]<=mid){
+           sum+=nums[i];
+           }
+           else
+           {
+            parts++;
+            sum=nums[i];
+           }
+        }
+        return parts<=k;
+         
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size(), sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        int low = 0, high = sum;
+        int ans = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (good(nums, mid, k)) {
+                ans=mid;
+                high = mid -1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
