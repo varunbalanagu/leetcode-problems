6e3bda1f36ea05b5495b20605d0b206d60461a93
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-05 11:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (92.82%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-     bool check(int row,int col,vector<vector<char>>&board,char c)
-    {
-        for(int i=0;i<9;i++)
-        {
-            if(i!=row&&board[i][col]==c)
-            {
-                return false;
-            }
-             if(i!=col&&board[row][i]==c)
-            {
-                return false;
-            }
-             int r = 3 * (row / 3) + i / 3;
-            int cc = 3 * (col / 3) + i % 3;
-            if((r!=row||cc!=col)&&board[r][cc]==c)
-            {
-                return false;
-            }
-          
+
+   bool check(int row,int col,vector<vector<char>>&board,char c ){
+    for(int i =0;i < 9;i++){
+        if(i!=row && board[i][col]==c){
+            return false;
         }
-          return true;
+        if(i!=col &&board[row][i]==c){
+            return false;
+        }
+        int rr=3*(row/3)+(i/3);
+        int cc=3*(col/3)+(i%3);
+        if(rr!=row && cc!=col && board[rr][cc]==c){
+            return false;
+        }
     }
-   
+    return true;
+   }
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
+        for(int i =0;i < 9;i++){
+            for(int j =0 ;j < 9;j++){
                 if(board[i][j]!='.'){
-                if(!check(i,j,board,board[i][j]))
-                {
-                   return false;
-                }
+                    if(!check(i,j,board,board[i][j]))
+                    {
+                        return false;
+                    }
                 }
             }
         }
         return true;
+        
     }
 };
