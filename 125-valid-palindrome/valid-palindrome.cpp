f6aab0d5eb0737dd4b9873d3eb51a6222097c084
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-05 11:16:46 +0530

    Time: 35 ms (5.38%) | Memory: 10 MB (54.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int  n = s.size();
-        string ans="";
-        // cout<<s[0]+32;
-        for(int i =0;i<n;i++){
-            if(s[i]>='A'&&s[i]<='Z'){
-                // cout<<s[i]<<endl;
-            s[i]=tolower(s[i]);
-            }
-             if((s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')){
-                // cout<<s[i]<<endl;
-               ans+=s[i];    
-            }
-            else{
+        int low = 0 ,high = s.size()-1;
+        while(low < high){
+            if(!isalpha(s[low])&&!isdigit(s[low])){
+                cout<<"hi";
+                low++;
                 continue;
             }
-            
-        }
-        cout<<ans<<endl;
-        int low =0 ,high = ans.size()-1;
-        while(low < high){
-            if(ans[low]!=ans[high]){
+            if(!isalpha(s[high])&&!isdigit(s[high])){
+                cout<<"hi";
+                high--;
+                continue;
+            }
+            else if(tolower(s[low])!=tolower(s[high])){
                 return false;
             }
-            low++; high--;
+            low++;
+            high--;
         }
         return true;
     }
