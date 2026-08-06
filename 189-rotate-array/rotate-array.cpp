3a74e01108ab4f642f290b305173fb373701b637
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-06 10:23:45 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (95.18%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,22 +2,9 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        deque<int>dq;
-        for(int i =0;i<n;i++){
-            dq.push_back(nums[i]);
-        }
-        int i =0;
-        while(i <k){
-         int n =dq.size();
-         int temp=dq[dq.size()-1];
-         dq.pop_back();
-         dq.push_front(temp);
-         i++;
-        }
-        nums.clear();
-        for(int i =0;i<dq.size();i++){
-            nums.push_back(dq[i]);
-        }
-        // nums=dq; 
+        k =k%n;
+       reverse(nums.begin(),nums.end());
+       reverse(nums.begin(),nums.begin()+k);
+       reverse(nums.begin()+k,nums.end());   
     }
 };
