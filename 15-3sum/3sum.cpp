bbf4113b805c73573cf90a9cd6e6fcedfe073240
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-12 13:43:29 +0530

    Time: 2547 ms (5.01%) | Memory: 550.8 MB (5.28%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        map<vector<int> ,int>mp;
+        map<vector<int>,int>mp;
+        vector<vector<int>>ans;
         for(int i =0;i<n;i++){
-            int low =i+1,high = n-1;
-            while(low < high){
-                if(nums[low]+nums[i]+nums[high]==0){
-                     if(mp.find({nums[i],nums[low],nums[high]})==mp.end()){
-                          ans.push_back({nums[i],nums[low],nums[high]});
-                          mp[{nums[i],nums[low],nums[high]}]++;
+                int low =i+1,high=n-1;
+                while(low<high){
+                    if(nums[low]+nums[high]+nums[i]==0){
+                        if(mp.find({nums[low],nums[high],nums[i]})==mp.end()){
+                            ans.push_back({nums[i],nums[low],nums[high]});
+                        }
+                            mp[{nums[low],nums[high],nums[i]}]++;
+                            low++;
+                            high--;
+                        }
+                        else if(nums[low]+nums[high]+nums[i]<0){
+                            low++;
+                        }
+                        else{
+                            high--;
+                        }
                     }
-                    low++;
-                    high--;
+
                 }
-                else if(nums[low]+nums[i]+nums[high]<0){
-                    low++;
-                }
-                else{
-                   high--;
-                }
-            }
+            
+            return ans;
         }
-        return ans;
-        
-    }
+       
 };
