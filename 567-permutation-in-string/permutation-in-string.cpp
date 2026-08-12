f2728167a3a765c46a13f2c571788fea8e13dd87
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-12 15:03:44 +0530

    Time: 1890 ms (5.04%) | Memory: 111.4 MB (5.18%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-   bool peremate(string ans,string s2){
-    sort(ans.begin(),ans.end());
-    sort(s2.begin(),s2.end());
-    return ans==s2;
-   }
+    bool permentation(string s1,string s2){
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        return s1==s2;
+    }
     bool checkInclusion(string s1, string s2) {
-        int n = s1.size();
-        int m =s2.size();
-        if(s1.size()>s2.size()) return false;
-        string ans="";
+        int n =s1.size();
+        int m=s2.size();
+        if(m<n) return false;
+        string str;
         for(int i =0;i<n;i++){
-            ans+=s2[i];
+          str.push_back(s2[i]);
         }
-        if(peremate(ans,s1)) return true;
-        for(int i =n;i < m;i++){
-            ans.erase(ans.begin());
-            ans+=s2[i];
-             if(peremate(ans,s1)) return true;
-
+        if(permentation(str,s1)) return true;
+        for(int i =n;i<m;i++){
+            str.erase(str.begin());
+            str.push_back(s2[i]);
+             if(permentation(str,s1)) return true;
         }
         return false;
     }
