b6d247f98bcf606c10877484035e475e9d9854d8
Varun Balangu <147472935+varunbalanagu@users.noreply.github.com>
2026-08-12 09:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (40.95%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>ans;
         int n = nums.size();
-        ans=nums;
-        for(int i =0;i<n;i++){
-            ans.push_back(nums[i]);
+        vector<int>ans;
+        for(int i =0;i<2*n;i++){
+            ans.push_back(nums[i%n]);
         }
         return ans;
+        
     }
 };
